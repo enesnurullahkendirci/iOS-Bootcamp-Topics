38b984be410ff691c48c775bb1f6f28fdff22daf
Enes Nurullah Kendirci <enesnkendirci@gmail.com>
2021-09-20 21:41:35 +0300

    Added toolbar and buttons(with methods) to webview. the bars were hidden while drag down.

--- BasicUIComponents/ViewControllers/WebViewContainerViewController.swift
+++ BasicUIComponents/ViewControllers/WebViewContainerViewController.swift
@@ -11,6 +11,10 @@ import WebKit
 class WebViewContainerViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var toolBar: UIToolbar!
+    
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var forwardButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad() // html content gosterelim font ve font buyuklugunu css ile degistirelim
@@ -42,6 +46,8 @@ class WebViewContainerViewController: UIViewController {
         
         webView.uiDelegate = self
         webView.navigationDelegate = self
+        
+        webView.scrollView.delegate = self // To use the scrollview delegate methods.
     }
     
     private var activityIndicatorView: UIActivityIndicatorView!
@@ -66,10 +72,28 @@ class WebViewContainerViewController: UIViewController {
             
             activityIndicatorView.isHidden = !webView.isLoading
         }
+        
+        // when it isn't avaible, buttons are NOT enable
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
+        
     }
-    @IBAction func reloadButtonTapped(_ sender: UIButton) {
-        webView.reload()
+    
+    //Method of toolbar buttons
+    @IBAction func toolbarButtonsTapped(_ sender: UIBarButtonItem) {
+        
+        switch sender.title {
+        case "back":
+            webView.goBack()
+        case "forward":
+            webView.goForward()
+        case "refresh":
+            webView.reload()
+        default:
+            return
+        }
     }
+    
 }
 
 extension WebViewContainerViewController: WKUIDelegate {
@@ -77,5 +101,19 @@ extension WebViewContainerViewController: WKUIDelegate {
 }
 
 extension WebViewContainerViewController: WKNavigationDelegate {
+    
+}
+
+extension WebViewContainerViewController: UIScrollViewDelegate{
+    
+    //
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+        let needHide = velocity.y >= 0 ? true : false // detect way of drag
+        
+        //hide bars
+        toolBar.isHidden = needHide
+        navigationController?.setNavigationBarHidden(needHide, animated: true)
+    }
     
 }
